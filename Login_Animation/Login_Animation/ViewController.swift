94e7cd841e42ce857411a7235d80bc5ab0f2cf5e
Kenny Batista <kennybatista7@gmail.com>
2016-04-14 09:23:58 -0400

    new proj

---
+++ Login_Animation/Login_Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login_Animation
+//
+//  Created by Student on 4/12/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
